877fb17394e19ef25c1be10acfa83e3a3454a043
Jan Allersma <janallersma@gmail.com>
2018-11-20 17:18:06 +0100

    Implement 'getEnrolledCourses()' in BBUserInfo.

--- src/api/BBUserInfo.ts
+++ src/api/BBUserInfo.ts
@@ -4,6 +4,7 @@ export default class BBUserInfo {
     private _userName;
 
     private userInfo: BBBackend.IUserInfo;
+    private enrolledCourses: BBBackend.ICourseID[];
 
     constructor(userName: string) {
         this._userName = userName;
@@ -27,6 +28,27 @@ export default class BBUserInfo {
             Backend.getBackend().getUserInfo(parameters).then((information) => {
                 this.userInfo = information;
                 resolve(this.userInfo);
+            });
+        });
+    }
+
+    public getEnrolledCourses(): Promise<BBBackend.ICourseID[]> {
+        return new Promise((resolve, reject) => {
+            if (this.enrolledCourses) {
+                resolve(this.enrolledCourses);
+                return;
+            }
+
+            this.getUserInfo().then((uinfo) => {
+                const parameters: BBBackend.EnrolledCoursesParameter = {
+                    offset: 0,
+                    userId: uinfo.id
+                };
+
+                Backend.getBackend().getEnrolledCourses(parameters).then((information) => {
+                    this.enrolledCourses = information;
+                    resolve(this.enrolledCourses);
+                });
             });
         });
     }
